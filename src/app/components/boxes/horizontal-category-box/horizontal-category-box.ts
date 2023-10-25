483342772697e1e3b43e22c20d38a57705542923
Asadbek_Azamjonov <asadbek.azamjonov@devkom.pl>
2023-10-25 23:56:58 +0200

    finished the tags feature

--- src/app/components/boxes/horizontal-category-box/horizontal-category-box.ts
+++ src/app/components/boxes/horizontal-category-box/horizontal-category-box.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/server_services/api.service';
 
 @Component({
@@ -9,12 +9,14 @@ import { ApiService } from 'src/app/server_services/api.service';
 export class HorizontalCategory {
 
 categories: any[] = [];
-  @ViewChild('tabsBox')tabsBox!: ElementRef;
-@ViewChild('arrowIcons') arrowIcons!: ElementRef;
-isDragging: boolean = false; 
-position = 0;
+@ViewChild('tabsBox') tabsBox: ElementRef | undefined;
+  @ViewChild('leftArrow') leftArrow: ElementRef | undefined;
+  @ViewChild('rightArrow') rightArrow: ElementRef | undefined;
+
+  scrollIndex = 0
   constructor(
     private apiService: ApiService,
+    private renderer: Renderer2
 
   ) { }
 
@@ -42,53 +44,36 @@ position = 0;
   
   
   }
-
-
-
-  ngAfterViewInit() {
-    // Initialize your component here
-    const tabsBox = this.tabsBox.nativeElement;
-    const allTabs = tabsBox.querySelectorAll('.tab');
-    const arrowIcons = this.arrowIcons.nativeElement.querySelectorAll('i');
-
-    const handleIcons = (scrollVal:any) => {
-      let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
-      arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? 'none' : 'flex';
-      arrowIcons[1].parentElement.style.display = maxScrollableWidth - scrollVal <= 1 ? 'none' : 'flex';
-    };
-
-    arrowIcons.forEach((icon:any) => {
-      icon.addEventListener('click', () => {
-        // if clicked icon is left, reduce 350 from tabsBox scrollLeft else add
-        let scrollWidth = (tabsBox.scrollLeft += icon.id === 'left' ? -340 : 340);
-        handleIcons(scrollWidth);
-      });
-    });
-
-    allTabs.forEach((tab :any)=> {
-      tab.addEventListener('click', () => {
-        tabsBox.querySelector('.active').classList.remove('active');
-        tab.classList.add('active');
-      });
-    });
-
-    const dragging = (e:any) => {
-      if (!this.isDragging) return;
-      tabsBox.classList.add('dragging');
-      tabsBox.scrollLeft -= e.movementX;
-      handleIcons(tabsBox.scrollLeft);
-    };
-
-    const dragStop = () => {
-      this.isDragging = false;
-      tabsBox.classList.remove('dragging');
-    };
-
-    tabsBox.addEventListener('mousedown', () => (this.isDragging = true));
-    tabsBox.addEventListener('mousemove', dragging);
-    document.addEventListener('mouseup', dragStop);
+ // Function to scroll the tabs to the left
+ scrollLeft() {
+  if (this.tabsBox && this.scrollIndex > 0) {
+    const scrollAmount = 100; // Adjust as needed
+    this.tabsBox.nativeElement.scrollLeft -= scrollAmount;
+    this.scrollIndex--; // Decrease scrollIndex when scrolling left
   }
+}
 
+// Function to scroll the tabs to the right
+scrollRight() {
+  if (this.tabsBox) {
+    const scrollAmount = 100; // Adjust as needed
+    this.tabsBox.nativeElement.scrollLeft += scrollAmount;
+    this.scrollIndex++; // Increase scrollIndex when scrolling right
+  }
+}
+
+// Add event listeners for arrow icons click events
+ngAfterViewInit() {
+  if (this.leftArrow && this.rightArrow) {
+    this.leftArrow.nativeElement.addEventListener('click', () => {
+      this.scrollLeft();
+    });
+
+    this.rightArrow.nativeElement.addEventListener('click', () => {
+      this.scrollRight();
+    });
+  }
+}
   onTabClick(tab:any) {
     this.categories.forEach(t => t.active = false); // Deselect all tabs
     tab.active = true; // Select the clicked tab
